05879436740c548ceecde0034159915816b41835
Christoph Maier <cm.hardware.software.elsewhere@gmail.com>
2017-10-13 01:43:17 +0200

    build app that auto starts sound file

--- app/src/main/java/com/example/android/soundoff/MainActivity.java
+++ app/src/main/java/com/example/android/soundoff/MainActivity.java
@@ -1,13 +1,17 @@
 package com.example.android.soundoff;
 
+import android.media.MediaPlayer;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 
 public class MainActivity extends AppCompatActivity {
-
+    private MediaPlayer playa;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        playa = MediaPlayer.create(this, R.raw.feinkost_zipp_uhr);
+        playa.start(); // no need to call prepare(); create() does that for you
     }
 }
