4cf5e039855a1af02e614c907eab2d65174ac7ee
Christoph Maier <cm.hardware.software.elsewhere@gmail.com>
2017-11-13 23:48:34 +0100

    load numbers ArrayList<String> and log its contents

--- app/src/main/java/com/example/android/miwok/NumbersActivity.java
+++ app/src/main/java/com/example/android/miwok/NumbersActivity.java
@@ -4,6 +4,8 @@ import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.util.Log;
 
+import java.util.ArrayList;
+
 public class NumbersActivity extends AppCompatActivity {
 
     @Override
@@ -11,7 +13,9 @@ public class NumbersActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_numbers);
 
-        String[] words = {"one", "two", "three", "four", "five", "six", "seven", "eight", "nine", "ten"};
-        for (int n = 0; n < words.length; Log.i("NumbersActivity","Word [" + n +"] is: " + words[n++]));
+        String[] word = new String[]{"one", "two", "three", "four", "five", "six", "seven", "eight", "nine", "ten"};
+        ArrayList<String> words = new ArrayList<String>(word.length);
+        for (int n = 0; n < word.length; words.add(word[n++]));
+        for (int n = 0; n < words.size(); Log.i("NumbersActivity","Word [" + n +"] is: " + words.get(n++)));
     }
 }
