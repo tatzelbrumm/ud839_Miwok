288e44615f1362322f06b823d47a35bbff2fb1a8
Christoph Maier <cm.hardware.software.elsewhere@gmail.com>
2017-11-13 23:32:58 +0100

    load numbers string array and log its contents

--- app/src/main/java/com/example/android/miwok/NumbersActivity.java
+++ app/src/main/java/com/example/android/miwok/NumbersActivity.java
@@ -2,6 +2,7 @@ package com.example.android.miwok;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.util.Log;
 
 public class NumbersActivity extends AppCompatActivity {
 
@@ -9,5 +10,8 @@ public class NumbersActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_numbers);
+
+        String[] words = {"one", "two", "three", "four", "five", "six", "seven", "eight", "nine", "ten"};
+        for (int n = 0; n < words.length; Log.i("NumbersActivity","Word [" + n +"] is: " + words[n++]));
     }
 }
