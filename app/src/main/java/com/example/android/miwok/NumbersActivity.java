2db2e0f3654897a34a6d5c0929a7f0b5bd233fdb
Christoph Maier <cm.hardware.software.elsewhere@gmail.com>
2017-11-14 01:24:26 +0100

    use ListView and ArrayAdapter

--- app/src/main/java/com/example/android/miwok/NumbersActivity.java
+++ app/src/main/java/com/example/android/miwok/NumbersActivity.java
@@ -2,9 +2,9 @@ package com.example.android.miwok;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
-import android.util.Log;
+import android.widget.ArrayAdapter;
 import android.widget.LinearLayout;
-import android.widget.TextView;
+import android.widget.ListView;
 
 import java.util.ArrayList;
 
@@ -19,11 +19,10 @@ public class NumbersActivity extends AppCompatActivity {
         ArrayList<String> words = new ArrayList<String>(word.length);
         for (int n = 0; n < word.length; words.add(word[n++]));
 
-        LinearLayout rootView = (LinearLayout) findViewById(R.id.rootView);
-        for (int n = 0; n < words.size(); n++) {
-            TextView tv = new TextView(this);
-            tv.setText(words.get(n));
-            rootView.addView(tv);
-        }
+        ArrayAdapter<String> itemsAdapter = new ArrayAdapter<String>(this, android.R.layout.simple_list_item_1, words);
+
+        ListView listView = (ListView) findViewById(R.id.list);
+
+        listView.setAdapter(itemsAdapter);
     }
 }
