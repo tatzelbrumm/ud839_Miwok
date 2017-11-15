d563ea18b0bf62fea70527103a90f99288a132bf
Christoph Maier <cm.hardware.software.elsewhere@gmail.com>
2017-11-15 16:14:43 +0100

    Toast at end of each Höllentrip sound file

--- app/src/main/java/com/example/android/miwok/MainActivity.java
+++ app/src/main/java/com/example/android/miwok/MainActivity.java
@@ -3,6 +3,7 @@ package com.example.android.miwok;
 import android.media.MediaPlayer;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
     private MediaPlayer playa;
@@ -12,6 +13,14 @@ public class MainActivity extends AppCompatActivity {
         setContentView(R.layout.activity_main);
 
         playa = MediaPlayer.create(this, R.raw.feinkost_zipp_uhr);
+
         playa.start(); // no need to call prepare(); create() does that for you
+        // OnCompletionListener needs to be set AFTER start of media player
+        playa.setOnCompletionListener(new MediaPlayer.OnCompletionListener() {
+            @Override
+            public void onCompletion(MediaPlayer mediaPlayer) {
+                Toast.makeText(MainActivity.this, "Danke. Kein Wirsing.", Toast.LENGTH_SHORT).show();
+            }
+        });
     }
 }
