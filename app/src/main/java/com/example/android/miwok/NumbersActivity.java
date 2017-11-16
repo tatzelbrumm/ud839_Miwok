4194dc39d7c446f10b6e7456024cd1bc8dee7033
Christoph Maier <cm.hardware.software.elsewhere@gmail.com>
2017-11-16 22:07:19 +0100

    Change NumbersActivity to use NumbersFragment, create activity_category.xml

--- app/src/main/java/com/example/android/miwok/NumbersActivity.java
+++ app/src/main/java/com/example/android/miwok/NumbersActivity.java
@@ -1,31 +1,16 @@
 package com.example.android.miwok;
 
+import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 
-import java.util.ArrayList;
+public class NumbersActivity extends AppCompatActivity {
 
-public class NumbersActivity extends VocabularyActivity {
-    /**
-     * onCreate sets up the dictionary list entries
-     * @param savedInstanceState
-     */
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-
-        // It is ESSENTIAL that words be declared final, so that they can be used by OnItemClickListener
-        final ArrayList<Word> words = new ArrayList<Word>();
-        words.add(new Word("one", "lutti", R.raw.number_one, R.drawable.number_one));
-        words.add(new Word("two", "otiiko", R.raw.number_two, R.drawable.number_two));
-        words.add(new Word("three", "tolookosu", R.raw.number_three, R.drawable.number_three));
-        words.add(new Word("four", "oyyisa", R.raw.number_four, R.drawable.number_four));
-        words.add(new Word("five", "massokka", R.raw.number_five, R.drawable.number_five));
-        words.add(new Word("six", "temmokka", R.raw.number_six, R.drawable.number_six));
-        words.add(new Word("seven", "kenekaku", R.raw.number_seven, R.drawable.number_seven));
-        words.add(new Word("eight", "kawinta", R.raw.number_eight, R.drawable.number_eight));
-        words.add(new Word("nine", "wo’e", R.raw.number_nine, R.drawable.number_nine));
-        words.add(new Word("ten", "na’aacha", R.raw.number_ten, R.drawable.number_ten));
-
-        fillList(this, words, R.color.category_numbers);
+        setContentView(R.layout.activity_category);
+        getSupportFragmentManager().beginTransaction()
+                .replace(R.id.container, new NumbersFragment())
+                .commit();
     }
 }
